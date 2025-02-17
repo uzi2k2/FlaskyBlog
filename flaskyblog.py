c590272d043b16ac05d68099a35c5a395b4d2db3
uzi2k2 <deppomixera@gmail.com>
2025-02-17 23:37:39 +0200

    Added a base layout template and updated home & about pages to extend that layout.

--- flaskyblog.py
+++ flaskyblog.py
@@ -20,13 +20,12 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-
     return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='about')
 
 
 if __name__ == "__main__":
